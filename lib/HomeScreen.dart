42dc1b97b85c51cd1f62712b5fb1300ec83bded6
debaprasad616 <121946181+debaprasad616@users.noreply.github.com>
2023-08-20 23:37:24 +0530

    Update HomeScreen.dart

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,36 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:wtf01/AppFooter.dart';
 import 'package:wtf01/AppHeader.dart';
+import 'package:wtf01/widgets/StoryWidget.dart';
+import 'package:wtf01/widgets/GossipChatWidget.dart'; // Import the new widget
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
+
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
-class _HomeScreenState extends State<HomeScreen> {
-  final PageController _pageController = PageController(initialPage: 0);
-  int _currentIndex = 0;
 
-  void _onTap(int index) {
-    setState(() {
-      _currentIndex = index;
-      _pageController.animateToPage(index,
-          duration: Duration(milliseconds: 500), curve: Curves.linear);
-    });
-    }
+class _HomeScreenState extends State<HomeScreen> {
+  final List<Map<String, String>> gossipData = [
+    {
+      'chatMessage': "Confess your crush in our class",
+      'replyCount': "17 replies",
+    },
+    {
+      'chatMessage': "Lets discuss about tomorrows exam",
+      'replyCount': "17 replies",
+    },
+    {
+      'chatMessage': "What is this, LMAO",
+      'replyCount': "17 replies",
+    },
+    {
+      'chatMessage': "I cant study for tomorrows exam what should I do",
+      'replyCount': "17 replies",
+    },
+    {
+      'chatMessage': "I can beat Pulkit in one on",
+      'replyCount': "17 replies",
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xfff2f1ec),
+      backgroundColor: Colors.white,
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           AppHeader(),
           Container(
-            // Search Bar
-            height: 40,
-            padding: EdgeInsets.symmetric(horizontal: 16),
-            margin: EdgeInsets.only(left: 20, right: 20, bottom: 10),
+            height: 40, // Adjusted height
+            margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             decoration: BoxDecoration(
               color: Color(0xFFC5CDCE),
               borderRadius: BorderRadius.circular(30),
@@ -38,119 +52,95 @@ class _HomeScreenState extends State<HomeScreen> {
             child: Row(
               children: [
                 Expanded(
-                  child: TextField(
-                    style: TextStyle(
-                      color: Color(0xFF667378),
-                      fontSize: 20,
-                    ),
-                    decoration: InputDecoration(
-                      hintText: 'Search anything...',
-                      hintStyle: TextStyle(
-                        color: Color(0xFF93A5AC),
-                        fontSize: 25,
-                        fontFamily: 'Cavet',
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: TextField(
+                      style: TextStyle(
+                        color: Color(0xFF667378),
+                        fontSize: 20,
                       ),
-                      border: InputBorder.none,
+                      decoration: InputDecoration(
+                        hintText: 'Search anything...',
+                        hintStyle: TextStyle(
+                          color: Color(0xFF93A5AC),
+                          fontSize: 20,
+                          fontFamily: 'Cavet',
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 16),
+                      ),
                     ),
                   ),
                 ),
-                Icon(
-                  Icons.search,
-                  color: Color(0xFF365B6D),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.search,
+                    color: Color(0xFF365B6D),
+                  ),
                 ),
               ],
             ),
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Container(
-                child: Column(
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "Latest Games",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Color(0xFF365B6D),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 300,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: latestGamesImagesList.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: EdgeInsets.all(20),
-                                child: Card(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.asset(
-                                      'assets/latestGamesImages/${latestGamesImagesList[index]}.png',
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "Updates",
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            color: Color(0xFF365B6D),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                          ),
-                        ),
-                        SizedBox(
-                          height: 300,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: updatesImagesList.length,
-                            itemBuilder: (context, index) {
-                              return Padding(
-                                padding: EdgeInsets.all(20),
-                                child: Card(
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10),
-                                    child: Image.asset(
-                                      'assets/updatesImages/${updatesImagesList[index]}.png',
-                                      fit: BoxFit.cover,
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Updates",
+              style: TextStyle(
+                color: Color(0xFF365B6D),
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
               ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return StoryWidget(imagePath: 'assets/image/storyImages/storyImage${(index % 3) + 1}.png');
+              },
+            ),
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              "Gossips",
+              style: TextStyle(
+                color: Color(0xFF365B6D),
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: gossipData.length,
+              itemBuilder: (context, index) {
+                return GossipChatWidget(
+                  chatMessage: gossipData[index]['chatMessage']!,
+                  replyCount: gossipData[index]['replyCount']!,
+                );
+              },
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your action here when the button is pressed/
+        },
+        child: Icon(
+          Icons.add,
+          size: 42, // Adjust the size as needed
+        ),
+        backgroundColor: Colors.green,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+
     );
   }
 }
-
-  List<String> latestGamesImagesList = ['pic1','pic2','pic4'];
-  List<String> updatesImagesList = ['pic1','pic2'];
-
